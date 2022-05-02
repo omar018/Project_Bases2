2fcbf17588d3fc87fb89a7884f46a861aa98cf62
daramirez97 <52076031+daramirez97@users.noreply.github.com>
2022-05-02 18:47:40 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+import multichain.command.*;
+import multichain.object.*;
+import java.util.List;
+
+/**
+ *
+ * @author Juan Felipe Peña
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        CommandManager commandManager = new CommandManager(
+                "localhost",
+                "8381",
+                "multichainrpc",
+                "5uHUWEMHdWsWXSpXDMug78oBtFdk8mMbBX8AUMQzX6sE"
+        );
+        
+        List<StreamKeyItem> items;
+        // Este programa imprime por consola la información de los datos almacenados en un stream
+        try {
+            commandManager.invoke(CommandElt.SUBSCRIBE, "Votos");
+            items = (List<StreamKeyItem>) commandManager.invoke(CommandElt.LISTSTREAMITEMS, 
+                    "Votos");
+            
+            for (StreamKeyItem item : items) {
+                System.out.println(item);
+            }
+        } catch (MultichainException e) {
+            e.printStackTrace();
+        }
+    }
+}
