7aff3a5c5d0b947b9537ca1f31c4748ae205766d
Omar Chavez <ochavez@unal.edu.co>
2022-05-03 19:23:16 -0500

    Agregué la parte de candidato

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import com.sun.source.tree.BindingPatternTree;
 import multichain.command.*;
 import multichain.object.*;
 import java.util.List;
@@ -15,8 +16,26 @@ public class Main {
                 "multichainrpc",
                 "5uHUWEMHdWsWXSpXDMug78oBtFdk8mMbBX8AUMQzX6sE"
         );
-        
+        List<StreamKeyItem> candidatos;
         List<StreamKeyItem> items;
+        try {
+            commandManager.invoke(CommandElt.SUBSCRIBE, "Candidatos");
+            candidatos = (List<StreamKeyItem>) commandManager.invoke(CommandElt.LISTSTREAMITEMS,//Obteniendo los candidatos del Stream
+                    "Candidatos");
+        } catch (MultichainException e) {
+            e.printStackTrace();
+        }
+
+        void voto(List<StreamKeyItem>){ //Método para imprimir lo que existe en el Stream de candidatos
+            for (StreamKeyItem candidato : candidatos) {
+                System.out.println(candidato.getKey()); //Número del candidato
+                System.out.println(candidato.getData()); //Aquí se obtienen los datos del candidato (supongo)
+        }
+        }
+
+
+
+
         // Este programa imprime por consola la información de los datos almacenados en un stream
         try {
             commandManager.invoke(CommandElt.SUBSCRIBE, "Votos");
